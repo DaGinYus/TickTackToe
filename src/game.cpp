7371da48c26c50b1c34eb2613a59bad46b9ef12a
Matthew Wong <matthew@adveca.com>
2018-05-29 14:12:52 -0700

    add function to draw the board

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 #include "game.h"
 
 GamePiece::GamePiece(int x, int y, char type) {
   m_coord [2] = {x, y};
   m_type = type;
+}
+
+Game::Game() {
+  // start with all blank spaces
+  for (int i=0; i<3; i++) {
+    for (int j=0; j<3; j++) {
+      board [i][j] = ' ';
+    }
+  }
+}
+
+void Game::refreshBoard() {
+  for (int i=0; i<3; i++) {
+    for (int j=0; j<3; j++) {
+      std::cout << '|' << board [i][j];
+    }
+    std::cout << std::endl;
+  }
 }
--- src/game.h
+++ src/game.h
@@ -1,23 +1,23 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <vector>
-
 class GamePiece {
  private:
   int m_coord [2]; // stored in x,y array
-  char m_type;
+  char m_type; // x or o
 
  public:
   GamePiece(int x, int y, char type);
-};
+}
 
 class Game {
  private:
-  std::vector<GamePiece> moveList;
+  char board [3][3];
+  std::vector<GamePiece> moveList; // remembers moves
 
  public:
-  Game();
-};
+  Game(); // creates new board and starts game
+  void refreshBoard(); // updates and draws to screen
+}
 
 #endif // GAME_H
