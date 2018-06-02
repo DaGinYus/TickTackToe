153286ded5dfed32a471fe138d22a89a2b081ece
Matthew Wong <matthew@adveca.com>
2018-06-01 17:32:33 -0700

    made changes to the way the board is drawn; added Board class

--- src/display.cpp
+++ src/display.cpp
@@ -1,41 +1,77 @@
 #include "display.h"
 
-WINDOW * createBoard() {
+/* WINDOW * createBoard() {
   initscr(); // initialize with one character at a time and echo suppression
   cbreak();
   noecho();
   keypad(stdscr, TRUE); // allows arrow key usage
 
   // start coordinates for screen
-  
-  WINDOW * win = newwin(LINES, COLS, 0, 0);
-  refresh();
-  
-  // draw the board
-  for (int i=0; i<5; i++) {
-    if (i % 2 == 0) {
-      for (int j=1; j<3; j++) {
-	mvwaddch(win, i, (j * 4), '|');
-      }
-    }
-    else if (i % 2 != 0) {
-      for (int j=1; j<12; j++) {
-	if (j % 4 == 0) {
-	  mvwaddch(win, i, j, '+');
-	}
-	else if (j % 4 != 0) {
-	  mvwaddch(win, i, j, '-');
-	}
-      }
-    }
-  }
+  startln = 2;
+  startcol = 3;
+  height = 13;
+  width = 25;
 
-  wrefresh(win);
-  return win;
+  
+  WINDOW * board = newwin(height, width, startln, startcol);
+  refresh();
+
+  box(board, 0, 0); // border
+
+  wchar_t ch = 0x2502;
+    
+  // draw the board
+  wmove(board, 1, 8);
+  wvline(board, ch, 11);
+  
+  wrefresh(board);
+  return board;
 }
 
-void destroyBoard(WINDOW * win) {
-  wrefresh(win);
+int * selectBox() {
+  return 0;
+}
+  
+
+void destroyBoard(WINDOW * board) {
+  wrefresh(board);
+  getch();
+  endwin();
+}
+*/
+
+Board::Board() {
+  startln = 2;
+  startcol = 3;
+  height = 5;
+  width = 9;
+}
+
+void Board::create() {
+  // ncurses options
+  initscr();
+  cbreak();
+  noecho();
+
+  // draw 3x3 box (width of each box is 9 and height is 5)
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      WINDOW * win = newwin(height, width, startln + (i * 4), startcol + (j * 8));
+      refresh();
+      board[i][j] = win;
+      box(board[i][j], 0, 0);
+      wrefresh(board[i][j]);
+    }
+  }
+}
+
+void Board::destroy() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      delwin(board[i][j]);
+      refresh();
+    }
+  }
   getch();
   endwin();
 }
--- src/display.h
+++ src/display.h
@@ -3,7 +3,24 @@
 
 #include <ncurses.h>
 
+class Board {
+ private:
+  WINDOW * board[3][3];
+  int width;
+  int height;
+  int startln;
+  int startcol;
+
+ public:
+  Board();
+  void create();
+  int * move();
+  void destroy();
+};
+   
+ 
 WINDOW * createBoard();
+int * selectBox();
 void destroyBoard(WINDOW * win);
 
 #endif // DISPLAY_H
