9423d34b4abcd224b6faf3fe715e761d1adda5af
Matthew Wong <matthew@adveca.com>
2020-04-29 23:47:47 -0700

    added player move function

--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+#include <ctype.h>
+
 #include "game.h"
+
+
+void Player::move() {
+  // asks player for move and updates board with player token
+  std::string move;
+  
+  std::cout << "Enter move: ";
+  std::cin >> move;
+  if (move.size() == 2) {
+    if (isalpha(move[0]) && isdigit(move[1])) {
+	// code for updating board goes here
+      break;
+    }
+  }
+  else {
+    std::cout << "Expected input: one letter and one number" << std::endl;
+  }
+}
+  
--- src/game.h
+++ src/game.h
@@ -1,2 +1,10 @@
-#pragma once
+#ifndef GAME_H
+#define GAME_H
 
+class Player {
+  char player_token;
+ public:
+  void move();
+};
+
+#endif // GAME_H
